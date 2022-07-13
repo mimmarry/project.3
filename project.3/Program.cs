1a578ddaa4ae46710b98a871735cf8f2ef652210
1 <1@LAPTOP-ILGMJ5ES>
2022-07-13 03:40:17 +0300

    Добавьте файлы проекта.

---
+++ project.3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Programm
+{
+    static void Main(string[] args)
+    {
+        double UsdToRub = 66.65;
+        double UsdToUah = 32.15;
+        double USD;
+
+        Console.WriteLine("Введите сумму в USD ");
+
+        USD = double.Parse(Console.ReadLine());
+
+        Console.WriteLine( USD +" USD in Rub = " + USD * UsdToRub);
+        Console.WriteLine( USD + " USD in Uah = " + USD * UsdToUah);
+
+    }
+}
